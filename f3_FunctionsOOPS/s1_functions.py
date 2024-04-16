2d187a47e1667db3d6678b7664bc2d4219e7751e
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-04-17 03:51:08 +0530

    started with functions

---
+++ f3_FunctionsOOPS/s1_functions.py
@@ -0,0 +1,12 @@
+def greet():
+    print("hello User")
+greet()
+
+#If we wish to call the function in a loop
+for i in range(9):
+    greet()
+
+
+def greet(n):
+    print("Hello "+n)
+greet("Vishwajit")
