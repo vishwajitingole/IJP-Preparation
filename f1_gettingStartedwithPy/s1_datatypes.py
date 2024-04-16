d94afb6cc32f7d84efba89a248217910cfbc7ec3
Vishwajeet Vikas Ingole <72921903+vishwajit022@users.noreply.github.com>
2024-04-16 19:53:35 +0530

    war started

---
+++ f1_gettingStartedwithPy/s1_datatypes.py
@@ -0,0 +1,17 @@
+#Integer
+print(10)
+print(type(10))
+
+
+print('-------------------------------')
+
+#Float
+print(10.2)
+print(type(10.2))
+print('-------------------------------')
+
+
+#String
+print("Hello")
+print(type("Hello"))
+print("-------------------------------")
